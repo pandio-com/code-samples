807fabe0fa822622314c180c36cdd38f00abc724
Kishan Dekivadiya <kishan@pandio.com>
2020-11-18 14:37:10 +0530

    PND-966 add websocket code for node js

---
+++ nodejs/websocket/consumer.js
@@ -0,0 +1,19 @@
+const pulsarURL = "{{ pulsar_url }}"
+const pulsarJWT = "{{ pulsar_jwt }}"
+const pulsarTopic = "persistent/public/default/topic-go-pandio-go/"
+const subscriptionName = "my-subscription-name"
+
+let WebSocket = require('ws'),
+    topic = pulsarURL + pulsarTopic + subscriptionName,
+    ws = new WebSocket(topic, {headers :{Authorization: "Bearer  "+ pulsarJWT}});
+
+ws.on('message', function(message) {
+    var receiveMsg = JSON.parse(message);
+    console.log('Received: %s - payload: %s', message, new Buffer(receiveMsg.payload, 'base64').toString());
+    var ackMsg = {"messageId" : receiveMsg.messageId};
+    ws.send(JSON.stringify(ackMsg));
+});
+
+ws.on("error", function(message) {
+    console.log('received error: %s', message);
+});
---
+++ nodejs/websocket/producer.js
@@ -0,0 +1,26 @@
+const pulsarURL = "{{ pulsar_url }}"
+const pulsarJWT = "{{ pulsar_jwt }}"
+const pulsarTopic = "persistent/public/default/topic-go-pandio-go"
+
+let WebSocket = require('ws'),
+    topic = pulsarURL + pulsarTopic,
+    ws = new WebSocket(topic, {headers :{Authorization: "Bearer  "+ pulsarJWT}});
+
+var message = {
+    "payload" : new Buffer("Hello World").toString('base64')
+};
+
+ws.on('open', function() {
+    // Send one message
+    ws.send(JSON.stringify(message));
+});
+
+ws.on('message', function(message) {
+    console.log('received ack: %s', message);
+    ws.close()
+});
+
+ws.on("error", function(message) {
+    console.log('received error: %s', message);
+});
+
