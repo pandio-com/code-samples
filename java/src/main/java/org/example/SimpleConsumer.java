a3df46fdcb1577b31a69225f8d4db645c45c4067
Charvak Patel <charvakcpatel007@gmail.com>
2020-12-03 19:02:51 +0530

    java_code_sample (#9)
    * Refine Websocket examples
    
    * java Code sample
    
    * java Code sample
    
    * Fix Java example
    
    * Remove duplicate declaration of PULSAR_TOPIC
    
    * Make java example consistent
    
    * Updated Readme for Java
    
    Co-authored-by: Rakshit Menpara <205111+deltasquare4@users.noreply.github.com>
    Co-authored-by: Rakshit Menpara <rakshit@pandio.com>

---
+++ java/src/main/java/org/example/SimpleConsumer.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.apache.pulsar.client.api.*;
+
+import java.util.concurrent.ExecutionException;
+
+public class SimpleConsumer {
+    public static String PULSAR_URL = "{{ pulsar_url }}";
+    public static String PULSAR_JWT = "{{ pulsar_jwt }}";
+    public static String PULSAR_TOPIC = "persistent://public/default/java-simple";
+    public static String PULSAR_SUBSCRIPTION_NAME = "sample";
+
+    public static void main(String[] argv) {
+
+        try (PulsarClient client = PulsarClient.builder()
+                .serviceUrl(PULSAR_URL)
+                .proxyServiceUrl(PULSAR_URL, ProxyProtocol.SNI)
+                .authentication(AuthenticationFactory.token(PULSAR_JWT))
+                .build()) {
+            try (Consumer<byte[]> consumer = client.newConsumer()
+                    .topic(PULSAR_TOPIC)
+                    .subscriptionName(PULSAR_SUBSCRIPTION_NAME)
+                    .subscribe()) {
+                Message<byte[]> message = consumer.receive();
+                try {
+                    System.out.printf("Message Received with Data : %s\n", new String(message.getData()));
+                    consumer.acknowledge(message);
+                } catch (Exception e ) {
+                    consumer.negativeAcknowledge(message);
+                    e.printStackTrace();
+                } finally {
+                    consumer.close();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/src/main/java/org/example/SimpleProducer.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.apache.pulsar.client.api.*;
+
+public class SimpleProducer {
+    public static String PULSAR_URL = "{{ pulsar_url }}";
+    public static String PULSAR_JWT = "{{ pulsar_jwt }}";
+    public static String PULSAR_TOPIC = "persistent://public/default/java-simple";
+
+    public static void main(String[] argv) throws PulsarClientException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl(PULSAR_URL)
+                .proxyServiceUrl(PULSAR_URL, ProxyProtocol.SNI)
+                .authentication(AuthenticationFactory.token(PULSAR_JWT))
+                .build();
+
+        Producer<byte[]> producer = client.newProducer()
+                .topic(PULSAR_TOPIC)
+                .create();
+
+        MessageId msgId = producer.send("Test Message".getBytes());
+
+        System.out.println("Message Published: " + msgId);
+        producer.close();
+        client.close();
+    }
+}
